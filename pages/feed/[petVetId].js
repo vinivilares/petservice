e903743393440ef975e85b2a760007a333b64fb0
Marcus Vinicius <marvi_cavalcanti@hotmail.com>
2022-10-06 13:35:22 -0300

    Tela pet/vet configurada

--- pages/feed/[petVetId].js
+++ pages/feed/[petVetId].js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { useRouter } from "next/router";
 import styles from "../../styles/petVetId.module.css";
 
@@ -6,21 +7,43 @@ export default function PetVetId({ petVetId }) {
 
   return (
     <div className={styles.main}>
-      <h1>{petVetId.dados.nome}</h1>
-      <p>{petVetId.dados.tipo}</p>
-      <h2>Endereço</h2>
-      <p>
-        Endereço: {petVetId.endereco.logradouro}, {petVetId.endereco.bairro},{" "}
-        {petVetId.endereco.localidade} - {petVetId.endereco.uf}
-      </p>
-      <p>CEP: {petVetId.endereco.cep}</p>
-      <h2>Serviços</h2>
-      <ul>
-        {petVetId.servicos.map((servico) => (
-          <li key={servico.id}>{servico.nmServ.NomeServico}</li>
-        ))}
-      </ul>
-      {console.log(petVetId)}
+      <div className={styles.top}>
+        <Image
+          src={"https://i.imgur.com/tdi3NGa.png"}
+          width={300}
+          height={300}
+          alt="Imagem do estabelecimento"
+          className={styles.img}
+        />
+        <div className={styles.dados}>
+          <h1>{petVetId.dados.nome}</h1>
+          <p className={styles.dadosTipo}>{petVetId.dados.tipo}</p>
+          <h2>Endereço</h2>
+          <p>
+            Endereço: {petVetId.endereco.logradouro}, {petVetId.endereco.bairro}
+            , {petVetId.endereco.localidade} - {petVetId.endereco.uf}
+          </p>
+          <p>CEP: {petVetId.endereco.cep}</p>
+        </div>
+      </div>
+
+      <div className={styles.servicos}>
+        <h2>Serviços</h2>
+        <ul className={styles.listServicos}>
+          {petVetId.servicos.map((servico) => (
+            <li key={servico.id}>
+              {/* <input
+                type={"checkbox"}
+                onChange={(target) => console.log(servico.id)}
+              /> */}
+              <p>{servico.nmServ.NomeServico}</p>
+              <p>{servico.descricao}</p>
+              <p>R$ {servico.preco}</p>
+            </li>
+          ))}
+        </ul>
+        {console.log(petVetId)}
+      </div>
     </div>
   );
 }
