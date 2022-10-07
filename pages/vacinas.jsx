f0e2e53ea7c3db6544c88480e06d2e7d16544248
Marcus Vinicius <marvi_cavalcanti@hotmail.com>
2022-10-07 19:09:33 -0300

    Api vacinas

--- pages/vacinas.jsx
+++ pages/vacinas.jsx
@@ -1,12 +1,8 @@
 import styles from "../styles/Vacinas.module.css";
+import { getSession } from "next-auth/react";
+import { buscarUser } from "../lib/prisma";
 
-export default function Vacinas() {
-  const vacinas = [
-    { id: 1, pet: "Totó", data: "02/02/10", tipo: "Antirrabica" },
-    { id: 2, pet: "Rex", data: "02/02/16", tipo: "Covid" },
-    { id: 3, pet: "Conan", data: "02/02/14", tipo: "Gripe" },
-    { id: 4, pet: "Dalila", data: "02/02/12", tipo: "Antitetânica" },
-  ];
+export default function Vacinas({ data }) {
   return (
     <div className={styles.main}>
       <h1>Histórico de Vacinas</h1>
@@ -21,18 +17,36 @@ export default function Vacinas() {
           </tr>
         </thead>
 
-        {vacinas.map((vacina) => (
-          <tr>
-            <td>{vacina.pet}</td>
-            <td>{vacina.data}</td>
-            <td>{vacina.tipo}</td>
-            <td className={styles.main}>
-              <button>Editar</button>
-              <button>Excluir</button>
-            </td>
-          </tr>
-        ))}
+        <tbody>
+          {data.map((item) =>
+            item.Vacina.map((vac) => (
+              <tr key={vac.id} className={styles.lista}>
+                <td>{item.nome}</td>
+                <td>{vac.nome}</td>
+                <td>{vac.data}</td>
+                <td>
+                  <button>Editar</button>
+                  <button>Excluir</button>
+                </td>
+              </tr>
+            ))
+          )}
+        </tbody>
       </table>
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const userSession = await getSession(context);
+  const user = await buscarUser(userSession.user.email);
+
+  const response = await fetch(`http://localhost:3000/api/vacinas/${user.id}`);
+  const data = await response.json();
+
+  return {
+    props: {
+      data: data,
+    },
+  };
 }
